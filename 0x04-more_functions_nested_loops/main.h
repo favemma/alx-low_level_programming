935e2d05cf8bcce96fbe15305ae9a194638f6dcb
Aborisade Ayobami <ayoaborisade@yahoo.com>
2022-09-15 16:18:15 -0700

    More Loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,7 +6,15 @@ int _putchar(char c);
 
 int _isdigit(int c);
 
+void print_triangle(int size);
+
+void print_square(int size);
+
 int mul(int a, int b);
+
+void print_line(int n);
+
+void print_diagonal(int n);
 
 void more_numbers(void);
 
