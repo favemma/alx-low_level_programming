8b58b98697ff5561a1cdd0c16065949c78c782c1
Aborisade Ayobami <ayoaborisade@yahoo.com>
2022-09-19 11:45:08 -0700

    function that swaps the values of two integers.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 
 /**
-* main - derefencing pointers
-*
-* Return: Always 0.
+* reset_to_98 - a function that takes a pointer to an int as parameter
+* and updates the value it points to to 98
+* @n: input
+* Return: n
 */
 
 void reset_to_98(int *n)
