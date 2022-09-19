353a47647b6a84d7d768305f66dea47249f85044
Aborisade Ayobami <ayoaborisade@yahoo.com>
2022-09-19 13:40:11 -0700

    function print half of a string follow by a newline

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,17 +16,18 @@ void puts_half(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 
+	{
+
 		len++;
 
 		n = (len / 2);
 
 	if ((len % 2) == 1)
 
-	n = ((len + 1) / 2);
+		n = ((len + 1) / 2);
+	}
 
-	for (i = n; str[i] != '\0'; i++)
-
-		_putchar(str[i]);
+	_putchar(str[i]);
 
 	_putchar('\n');
 
