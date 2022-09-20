162e204b50d7f9203612db0744a302b968b50721
Aborisade Ayobami <ayoaborisade@yahoo.com>
2022-09-19 17:08:57 -0700

    Pointers and Array

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -21,3 +21,23 @@ void puts_half(char *str)
 	_putchar('\n');
 
 }
+
+/**
+* _sstrlen - returns the length of a string
+* @s:  string
+*
+* Returns: the length of the given string
+*/
+int _strlen(char *s)
+{
+
+	int len = 0;
+
+	while (*(s + len) != '\0')
+
+		len++;
+
+	return (len);
+
+}
+
