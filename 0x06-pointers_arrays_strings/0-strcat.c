33be7fea7e983304c76fe67e217c836c5bca7129
Aborisade Ayobami <ayoaborisade@yahoo.com>
2022-09-21 12:46:19 -0700

    Concatenates 2 strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,10 +10,47 @@
 char *_strcat(char *dest, char *src)
 {
 
-	strcat = (*dest,*src);
+	int i, j;
 
-	printf("Concatenated String: %s\n", *dest);
+	i = strlen(*dest);
 
-	return 0;
+	for (j = 0; *src[j] != '\0'; i++, j++) {
+
+		*dest[i] = *src[j];
+
+	}
+
+	*dest[i] = '\0';
+
+}
+
+/**
+
+* _strlen - a function that returns the length of a string
+
+* @s: char input
+
+* Return: length of the input string
+
+*/
+
+int _strlen(char *s)
+{
+
+	int i = 1, sum = 0;
+
+	char pl = s[0];
+
+	while (pl != '\0')
+
+	{
+
+	sum++;
+
+	pl = s[i++];
+
+	}
+
+	return (sum);
 
 }
