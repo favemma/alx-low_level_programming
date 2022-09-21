6bf3ee80ae61ed3efd50c7e599b0a37444bf8ce1
Aborisade Ayobami <ayoaborisade@yahoo.com>
2022-09-21 14:21:01 -0700

    Concatenate 2 strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,13 +12,13 @@
 char *_strcat(char *dest, char *src)
 {
 
-	int i = 0, dest_len= 0;
+	int i = 0, dest_len = 0;
 
 	while (dest[i++])
-	
-		dest_len++;
 
-	for(i = 0; src[i]; i++)
+	dest_len++;
+
+	for (i = 0; src[i]; i++)
 
 		*dest[dest_len++] = *src[i];
 
