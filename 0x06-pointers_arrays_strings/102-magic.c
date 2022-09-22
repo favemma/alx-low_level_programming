eb6b827c1acbe25a4d1ca29e114edf954fbadebf
Aborisade Ayobami <ayoaborisade@yahoo.com>
2022-09-22 14:35:26 -0700

    Pointers Arrays and Strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
 int main(void)
-
 {
-
 	int n;
-
    	int a[5];
-
 	int *p;
 
 	a[2] = 1024;
-
 	p = &n;
-
 /**
 * write your line of code here...
 * Remember:
@@ -22,8 +16,7 @@ int main(void)
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-
-	*(p + 5) = 98;
+	*(p + 2) = 98;
 
 /** ...so that this prints 98\n */
 
