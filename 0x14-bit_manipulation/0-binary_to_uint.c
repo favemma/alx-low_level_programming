45c1d515c3448fedb12afac699d3209dde80475a
favemma <ayoaborisade@yahoo.com>
2022-10-27 14:56:35 -0700

    function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,52 +1,34 @@
-#include "main.h"
+#include <stddef.h>
 
 /**
-* _stoi - converts chars to ints
-* @c: char to convert
-* Return: converted int
-*/
-
-unsigned int _stoi(char c)
-{
-	return ((unsigned int) c - '0');
-}
-
-/**
-* _strlen - calculates the length of the string
-* @s: input
-* Return: length of string
-*/
-
-unsigned int _strlen(const char *s)
-(
-	unsigned int i;
-	
-	for (i = 0; s[i]; i++)
-		;
-	return (i);
-}
-
-/**
-* binary_to_uint - converts a string of 1's and 0's to a decimal number
+* binary_to_uint - converts a bit string to unsigned int
+*
 * @b: string to convert
-* Return: unsigned decimal number
+*
+* Return: decimal unsigned int value
 */
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int result, tmp, expo;
+	unsigned int n;
 	
-	if (!b)
+	if (b == NULL)
 		return (0);
-	result = tmp = 0;
-	expo = 1;
-	for (i = _strlen(b) - 1; b[i]; i--, expo *= 2)
+	for (n = 0; *b != 0; b++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (*b == '0')
+		{
+			n = n << 1;
+		}
+		else if (*b == '1')
+		{
+			n = n << 1;
+			n++;
+		}
+		else
+		{
 			return (0);
-		tmp = _stoi(b[i]);
-		result += tmp * expo;
+		}
 	}
-	return (result);
+	return (n);
 }
